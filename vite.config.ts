1391e85fc6b3fd96ed80cebc54a09de942fc1239
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-14 18:10:16 +0300

    add eslint packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
-import * as path from "path";
+import * as path from 'path';
+import eslint from 'vite-plugin-eslint';
 
 export default ({ mode }) => {
-	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-// https://vitejs.dev/config/
-return defineConfig({
-	resolve: {
-		alias: {
-			'@app': path.resolve(__dirname, './src/app'),
-			'@processes': path.resolve(__dirname, './src/processes'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@widgets': path.resolve(__dirname, './src/widgets'),
-			'@features': path.resolve(__dirname, './src/features'),
-			'@entities': path.resolve(__dirname, './src/entities'),
-			'@shared': path.resolve(__dirname, './src/shared'),
+	// https://vitejs.dev/config/
+	return defineConfig({
+		resolve: {
+			alias: {
+				'@app': path.resolve(__dirname, './src/app'),
+				'@processes': path.resolve(__dirname, './src/processes'),
+				'@pages': path.resolve(__dirname, './src/pages'),
+				'@widgets': path.resolve(__dirname, './src/widgets'),
+				'@features': path.resolve(__dirname, './src/features'),
+				'@entities': path.resolve(__dirname, './src/entities'),
+				'@shared': path.resolve(__dirname, './src/shared'),
+			},
 		},
-	},
-	plugins: [react()],
-});
-}
+		plugins: [react(), eslint()],
+	});
+};
