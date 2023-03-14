34b3b303a550a3aec4e712d20ddf62f426232daf
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-14 06:17:05 +0300

    add path shortcuts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import * as path from "path";
+
+export default ({ mode }) => {
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+return defineConfig({
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, './src/app'),
+			'@processes': path.resolve(__dirname, './src/processes'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+		},
+	},
+	plugins: [react()],
+});
+}
