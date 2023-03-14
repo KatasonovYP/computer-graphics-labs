d4bef38c709ebe75281a83478221127d13ddaf63
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-14 06:28:48 +0300

    style routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,19 @@ import { Route, Routes } from 'react-router';
 import { lazy } from 'react';
 
 const Home = lazy(() => import('./Home'));
-const Lab01 = lazy(() => import("./lab-01"));
+const Lab02 = lazy(() => import('./lab-02'));
 
 export const Routing = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/lab-01" element={<Lab01 />} />
+			<Route
+				path="/"
+				element={<Home />}
+			/>
+			<Route
+				path="/lab-01"
+				element={<Lab02 />}
+			/>
 		</Routes>
 	);
 };
