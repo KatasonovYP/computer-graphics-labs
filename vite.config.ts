a74e7a2d8ff4ba130fc9533b8b41b1fdca989b5d
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-16 01:18:31 +0300

    add/fix configs for linter, ts, prettier, vite, package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
-import * as path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import eslint from 'vite-plugin-eslint';
 
 export default ({ mode }) => {
@@ -8,17 +8,7 @@ export default ({ mode }) => {
 
 	// https://vitejs.dev/config/
 	return defineConfig({
-		resolve: {
-			alias: {
-				'@app': path.resolve(__dirname, './src/app'),
-				'@processes': path.resolve(__dirname, './src/processes'),
-				'@pages': path.resolve(__dirname, './src/pages'),
-				'@widgets': path.resolve(__dirname, './src/widgets'),
-				'@features': path.resolve(__dirname, './src/features'),
-				'@entities': path.resolve(__dirname, './src/entities'),
-				'@shared': path.resolve(__dirname, './src/shared'),
-			},
-		},
-		plugins: [react(), eslint()],
+		resolve: {},
+		plugins: [react(), tsconfigPaths(), eslint()],
 	});
 };
