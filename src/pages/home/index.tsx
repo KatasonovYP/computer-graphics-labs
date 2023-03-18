4df0b9f6ad83181b09da3f761645cfc14e3b1309
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-18 11:31:44 +0300

    update homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
 import { type FC } from 'react';
+import { Heading, SimpleGrid } from '@chakra-ui/react';
 
 export const Home: FC = () => {
 	return (
-		<div>
-			<div>home</div>
-		</div>
+		<SimpleGrid
+			m='auto'
+			maxW={'fit-content'}
+			columns={[1, null, 2]}
+			spacing={4}
+		>
+			<Heading size={'2xl'}>Home</Heading>
+		</SimpleGrid>
 	);
 };
