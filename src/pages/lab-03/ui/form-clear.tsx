c27cad6ac73397aed435969fa1e80a5bbcd104d5
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-03-26 19:08:05 +0300

    simplify clear button

--- src/pages/lab-03/ui/form-clear.tsx
+++ src/pages/lab-03/ui/form-clear.tsx
@@ -1,39 +1,9 @@
 import { type FC } from 'react';
-import { type SubmitHandler, useForm } from 'react-hook-form';
-import { Button, SimpleGrid, Stack } from '@chakra-ui/react';
-
-import { NumberInput, SubmitButton } from 'shared/components';
-import { onPromise } from 'shared/lib';
+import { Button } from '@chakra-ui/react';
 
 import { useLinesStore } from '../store/lines-store';
 
-interface IRotateLineForm {
-	angle: number;
-}
-
 export const FormClear: FC = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<IRotateLineForm>();
-
 	const clear = useLinesStore((state) => state.clear);
-	// eslint-disable-next-line unicorn/consistent-function-scoping
-	const onAction: SubmitHandler<IRotateLineForm> = (data): void => {
-		clear();
-	};
-
-	return (
-		<form onSubmit={onPromise(handleSubmit(onAction))}>
-			<SimpleGrid
-				m='auto'
-				maxW={'fit-content'}
-				columns={[1, null, 2]}
-				spacing={4}
-			>
-				<SubmitButton>Очистить</SubmitButton>
-			</SimpleGrid>
-		</form>
-	);
+	return <Button onClick={clear}>Очистить</Button>;
 };
