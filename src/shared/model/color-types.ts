6a3f54145b527fb661af3a67c189d959993f5c0c
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-04-04 01:10:03 +0300

    decompose color types

--- src/shared/model/color-types.ts
+++ src/shared/model/color-types.ts
@@ -2,3 +2,10 @@ import { type theme as chakraTheme } from '@chakra-ui/theme';
 
 export type ColorToken = keyof typeof chakraTheme.colors;
 export type ColorShade = keyof (typeof chakraTheme.colors)[ColorToken];
+
+export interface Irgba {
+	r: number;
+	g: number;
+	b: number;
+	a: number;
+}
--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,4 +1,4 @@
 export { EPanel } from './ui-types';
 export { EAction, type IFrame } from './history-types';
 export type { IPoint, IEdge } from './geometry-types';
-export type { ColorToken, ColorShade } from './color-types';
+export type { ColorToken, ColorShade, Irgba } from './color-types';
