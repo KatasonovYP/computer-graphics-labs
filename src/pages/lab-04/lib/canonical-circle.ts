c4503a53beefcae8f7c8c503e266b42d2a8a918c
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-04-04 23:31:48 +0300

    fix border in canonical-circle.ts

--- src/pages/lab-04/lib/canonical-circle.ts
+++ src/pages/lab-04/lib/canonical-circle.ts
@@ -7,7 +7,7 @@ export function canonicalCircle(center: IPosition, radius: number, color: Irgba)
 
 	const border = Math.round(center.x + radius / Math.sqrt(2));
 
-	for (let x = 0; x < border; ++x) {
+	for (let x = center.x; x <= border; ++x) {
 		const y = center.y + Math.sqrt(radius ** 2 - (x - center.x) ** 2);
 		pixels.push(new Pixel(x, y, color));
 	}
