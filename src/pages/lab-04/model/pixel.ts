11c1773c1becc837a65bce34d5eb3fbbc3a776b1
kyup21u270 <kyup21u270@bmstu.student.ru>
2023-04-09 21:34:51 +0300

    remove constants from draw pixel

--- src/pages/lab-04/model/pixel.ts
+++ src/pages/lab-04/model/pixel.ts
@@ -1,15 +1,13 @@
 import { type Irgba } from 'shared/model';
 
-import { HEIGHT, WIDTH } from 'shared/config';
-
 import { type IPosition } from './position';
 
-function isOnCanvas(pixel: Pixel, centerPosition: IPosition): boolean {
+function isOnCanvas(pixel: Pixel, centerPosition: IPosition, image: ImageData): boolean {
 	return (
 		pixel.x + centerPosition.x >= 0 &&
-		pixel.x + centerPosition.x <= WIDTH &&
+		pixel.x + centerPosition.x <= image.width &&
 		pixel.y + centerPosition.y >= 0 &&
-		pixel.y + centerPosition.y <= HEIGHT
+		pixel.y + centerPosition.y <= image.height
 	);
 }
 
@@ -25,7 +23,7 @@ export class Pixel {
 	}
 
 	draw(image: ImageData, centerPosition: IPosition): void {
-		if (isOnCanvas(this, centerPosition)) {
+		if (isOnCanvas(this, centerPosition, image)) {
 			const roundedX = Math.round(centerPosition.x + this.x);
 			const roundedY = Math.round(centerPosition.y + this.y);
 			const index = 4 * (image.width * roundedY + roundedX);
@@ -33,8 +31,8 @@ export class Pixel {
 			const data = image.data;
 			data[index] = this.color.r;
 			data[index + 1] = this.color.g;
-			data[index + 3] = this.color.a;
 			data[index + 2] = this.color.b;
+			data[index + 3] = this.color.a;
 		}
 	}
 }
